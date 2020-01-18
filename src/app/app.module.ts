e41f4b0abc4f6c3115f57a06cdf40a47090df41d
ChristianNelby <christiannelby@hotmail.com>
2020-01-18 19:40:14 +0100

    Oprettet components, service og hentet bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { InfoComponent } from './home/info/info.component';
+import { SearchComponent } from './home/search/search.component';
+import { FilteringComponent } from './home/filtering/filtering.component';
+import { TableOverviewComponent } from './home/table-overview/table-overview.component';
+import { ReservationComponent } from './home/reservation/reservation.component';
+import { ModalComponent } from './home/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    InfoComponent,
+    SearchComponent,
+    FilteringComponent,
+    TableOverviewComponent,
+    ReservationComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
