837b96ad749479ef2d5314ef5cf5dbf7247e57e6
Mads Nielsen <mads_nielsen9@hotmail.com>
2020-01-18 22:15:10 +0100

    Info and search bar layouts

--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  fromTime = {hour: 13, minute: 30};
+  fromSpinners = false;
+  fromSize = "small";
+  toTime = {hour: 14, minute: 30};
+  toSpinners = false;
+  toSize = "small";
+
+  searchText:string = "";
+  now:boolean = true; 
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  
 
 }
